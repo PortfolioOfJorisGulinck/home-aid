36baade7690f45865f3f637e3e052cf2eb70aa03
JorisOpKdg <35931513+JorisOpKdg@users.noreply.github.com>
2021-09-12 17:58:12 +0200

    Setting up the spring boot app

---
+++ src/test/java/be/jorisgulinck/homeaid/HomeAidApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jorisgulinck.homeaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
